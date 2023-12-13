d0abed555a7689b373ac584433b712e3037ebc72
Oleg <sir.serow@gmail.com>
2023-12-13 13:19:29 +0900

    added automatic script start

---
+++ server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Start the Flask application
+python3 /home/olec/smart_switch/src/app.py &
+
+# Wait for the server to start (adjust the sleep duration if needed)
+sleep 5
+
+# Open the default browser (if a display is available)
+if [ -n "$DISPLAY" ]; then
+    xdg-open http://0.0.0.0:5000/
+fi

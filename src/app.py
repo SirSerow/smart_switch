1953eda429155f01adcae60368660724ab330c3a
Oleg <sir.serow@gmail.com>
2023-12-14 17:44:37 +0900

    Current action change

--- src/app.py
+++ src/app.py
@@ -83,15 +83,11 @@ def execute_action(message):
         print('Executing action')
         # You can define the action based on your application logic.
         # For example, you can check some condition and execute different actions accordingly.
-        action = current_action  # Replace with the appropriate action
-        if action == 'call':
-            current_action = 'call'
+        if current_action == 'call':
             return make_skype_call("skype_username_or_phone_number") 
-        elif action == 'mail':
-            current_action = 'mail'
+        elif current_action == 'mail':
             return send_email("recipient@example.com", "Test Email", "This is a test email.")
-        elif action == 'link':
-            current_action = 'link'
+        elif current_action == 'link':
             return open_website("https://synkom.co.jp/")
         else:
             return 'Unknown action'

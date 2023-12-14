4b9af4d4b421503473e9e00a52e408f985f1f393
Oleg <sir.serow@gmail.com>
2023-12-14 10:30:52 +0900

    changed browser

--- server.sh
+++ server.sh
@@ -8,16 +8,5 @@ sleep 5
 
 # Open the default browser (if a display is available)
 if [ -n "$DISPLAY" ]; then
-    
-    # Start Chromium in kiosk mode
-    chromium-browser --app=http://0.0.0.0:5000/ --start-fullscreen --kiosk --start-maximized & 
-
-    # Wait for Chromium to open (adjust the sleep duration if needed)
-    sleep 15
-
-    # Simulate F11 keypress to maximize the window
-    xdotool key F11
-
-    # Hide the mouse cursor using unclutter
-    #unclutter -idle 0.1 -root &
+    epiphany-browser -a --profile ~/.config http://0.0.0.0:5000/
 fi

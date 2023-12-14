8c5e5d079d723e8d7965e13563166b87686b0a6b
Oleg <sir.serow@gmail.com>
2023-12-14 09:52:37 +0900

    changed start script

--- server.sh
+++ server.sh
@@ -8,14 +8,15 @@ sleep 5
 
 # Open the default browser (if a display is available)
 if [ -n "$DISPLAY" ]; then
-    # Simulate F11 keypress to maximize the window
-    xdotool key F11
-
+    
     # Start Chromium in kiosk mode
     chromium-browser --app=http://0.0.0.0:5000/ --start-fullscreen --kiosk --start-maximized &
 
     # Wait for Chromium to open (adjust the sleep duration if needed)
     sleep 5
+
+    # Simulate F11 keypress to maximize the window
+    xdotool key F11
 
     # Hide the mouse cursor using unclutter
     unclutter -idle 0.1 -root &

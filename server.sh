d6f80480183a4862b8d1d2bb3baf109911677acd
Oleg <sir.serow@gmail.com>
2023-12-14 11:18:43 +0900

    debug

--- server.sh
+++ server.sh
@@ -4,7 +4,7 @@
 python3 /home/olec/smart_switch/src/app.py &
 
 # Wait for the server to start (adjust the sleep duration if needed)
-sleep 5
+sleep 30
 
 # Open the default browser (if a display is available)
 if [ -n "$DISPLAY" ]; then

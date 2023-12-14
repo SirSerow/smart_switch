8189b078cfed4f7b9403a8ebf4581c57c2282c53
Oleg <sir.serow@gmail.com>
2023-12-14 09:45:26 +0900

    changed script

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Start the Flask application
 python3 /home/olec/smart_switch/src/app.py &
 
@@ -7,12 +8,15 @@ sleep 5
 
 # Open the default browser (if a display is available)
 if [ -n "$DISPLAY" ]; then
+    # Simulate F11 keypress to maximize the window
+    xdotool key F11
+
+    # Start Chromium in kiosk mode
     chromium-browser --app=http://0.0.0.0:5000/ --start-fullscreen --kiosk --start-maximized &
 
-    
     # Wait for Chromium to open (adjust the sleep duration if needed)
     sleep 5
 
-    # Use xdotool to simulate the F11 keypress to maximize the window
-    xdotool key F11
+    # Hide the mouse cursor using unclutter
+    unclutter -idle 0.1 -root &
 fi

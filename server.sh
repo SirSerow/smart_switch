4c56c72da744cdfbb99ccb85ada5f2586472e7cd
Oleg <sir.serow@gmail.com>
2023-12-14 11:29:09 +0900

    updated browser to firefox

--- server.sh
+++ server.sh
@@ -4,9 +4,11 @@
 python3 /home/olec/smart_switch/src/app.py &
 
 # Wait for the server to start (adjust the sleep duration if needed)
-sleep 30
+sleep 5
 
 # Open the default browser (if a display is available)
 if [ -n "$DISPLAY" ]; then
-    epiphany-browser -a --profile ~/.config http://0.0.0.0:5000/
+    firefox-esr -fullscreen http://0.0.0.0:5000/ &
+    sleep 5 # Adjust the sleep duration as needed
+    xdotool search --onlyvisible --class "Firefox" windowactivate --sync key F11
 fi
